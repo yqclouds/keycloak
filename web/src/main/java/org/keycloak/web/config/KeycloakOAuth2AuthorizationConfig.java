55b47ef86573ea98929e0768b7cdd36b738532ba
zhanhongbo1112 <eric..zhan@163.com>
2020-03-24 08:24:19 +0800

    web module

---
+++ web/src/main/java/org/keycloak/web/config/KeycloakOAuth2AuthorizationConfig.java
@@ -0,0 +1,10 @@
+package org.keycloak.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class KeycloakOAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+}
---
+++ web/src/main/java/org/keycloak/web/config/KeycloakSecurityConfig.java
@@ -0,0 +1,29 @@
+package org.keycloak.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class KeycloakSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.userDetailsService(userDetailsService());
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/oauth/**").permitAll()
+                .anyRequest().authenticated();
+    }
+}
