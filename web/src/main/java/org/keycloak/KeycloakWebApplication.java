1f8e54b6cb5e079dbd3c97d1a0a6f44e49888f50
zhanhongbo1112 <eric..zhan@163.com>
2020-03-25 23:18:34 +0800

    web module

--- web/src/main/java/org/keycloak/KeycloakWebApplication.java
+++ web/src/main/java/org/keycloak/KeycloakWebApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KeycloakApplication {
+public class KeycloakWebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(KeycloakApplication.class, args);
+        SpringApplication.run(KeycloakWebApplication.class, args);
     }
 }
