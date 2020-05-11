766e69bbce70c74148d92ec43ddb64fb7eeb0664
zhanhongbo1112 <eric..zhan@163.com>
2020-05-11 23:09:25 +0800

    remove unnecessary modules

--- model/infinispan/src/main/java/org/keycloak/models/cache/infinispan/entities/InClient.java
+++
@@ -1,9 +0,0 @@
-package org.keycloak.models.cache.infinispan.entities;
-
-/**
- * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
- * @version $Revision: 1 $
- */
-public interface InClient extends InRealm {
-    String getClientId();
-}
--- model/infinispan/src/main/java/org/keycloak/models/cache/infinispan/entities/InRealm.java
+++
@@ -1,9 +0,0 @@
-package org.keycloak.models.cache.infinispan.entities;
-
-/**
- * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
- * @version $Revision: 1 $
- */
-public interface InRealm extends Revisioned {
-    String getRealm();
-}
